0cbac1c30befad604068861501db79761af4b139
yvoloshchuk <yaroslav.voloshchuk@grammarly.com>
2014-09-13 02:54:26 +0300

    vk parser fix

--- packages/vk-music-parse/package.js
+++ packages/vk-music-parse/package.js
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
 
   // api.use(['http'], ['server']);
 
-  api.addFiles('vk-music-parse.js');
+  api.addFiles('vk-music-parse.js', ['server']);
   // api.export(['extendPost']);
 });
 
